f8c552f9fce94f2797afce63d912a294611e66c5
rodrigcasio <rod.casio@outlook.com>
2025-11-06 18:50:23 -0600

    Finished added `3rd test` for practicing try.. except with `except Exception as e` for catching errors during mathematical calculations

--- courseraPractice/python101/exceptionHandlingEx.py
+++ courseraPractice/python101/exceptionHandlingEx.py
@@ -1,3 +1,5 @@
+# Exception Handling Practice 
+#
 # Try except:
 
 def save_divide(num, den):
@@ -12,8 +14,8 @@ def save_divide(num, den):
         print(f"Process Complete")
 # test
 
-numerator = int(input('Enter the numerator value: -'))
-denominator = int(input('Enter the denominator value: -'))
+numerator = int(input('Enter the numerator value: '))
+denominator = int(input('Enter the denominator value: '))
 
 
 print(save_divide(numerator, denominator))
@@ -24,14 +26,36 @@ import math
 
 def squareR(num):
     try:
-        return math.sqrt(num)
+        result = math.sqrt(num)
+        print(f"Result: {result}")
     except ValueError:
         print(f"Cannot get the square root of a negative number. Please try again")
     else:
-        print(f"Something went wrong")
+        print(f"No error encountered")
     finally:
         print(f"Process Complete")
 
-x = int(input('Enter the value of x: '))
-print(squareR(x))
+x = float(input('Enter the value of x: '))
+squareR(x)
+
+# third test
+
+def cal(num2):
+    try:
+        result = num2 // (num2 - 5)
+        print(f"Result: {result}")
+    except Exception as e:
+        print(f"Error occurred during calculation")
+    except ValueError:
+        print('Invalid value')
+    except ZeroDivisionError:
+        print('Error: Cannot divide by zero')
+    else:
+        print(f"No error encountered")
+    finally:
+        print(f"Process Finished")
+
+y = int(input(f"Place the value of y: "))
+cal(y)
+
 
