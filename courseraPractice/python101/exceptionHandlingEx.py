40cf923118093406680b6333c35cdcbeb9397f43
rodrigcasio <rod.casio@outlook.com>
2025-11-06 18:26:15 -0600

    Added second test for getting the square root of number placed in function `squareR()`

--- courseraPractice/python101/exceptionHandlingEx.py
+++ courseraPractice/python101/exceptionHandlingEx.py
@@ -6,7 +6,10 @@ def save_divide(num, den):
     except ZeroDivisionError:
         print('Error: cannot divide by zero')
         return none
-
+    else:
+        print(f"Something went wrong")
+    finally:
+        print(f"Process Complete")
 # test
 
 numerator = int(input('Enter the numerator value: -'))
@@ -14,3 +17,21 @@ denominator = int(input('Enter the denominator value: -'))
 
 
 print(save_divide(numerator, denominator))
+
+# second test
+
+import math
+
+def squareR(num):
+    try:
+        return math.sqrt(num)
+    except ValueError:
+        print(f"Cannot get the square root of a negative number. Please try again")
+    else:
+        print(f"Something went wrong")
+    finally:
+        print(f"Process Complete")
+
+x = int(input('Enter the value of x: '))
+print(squareR(x))
+
