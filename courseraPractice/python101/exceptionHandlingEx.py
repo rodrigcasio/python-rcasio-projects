e9ba1ac3d9bef8faa52e46f04c1bc3ab5a7150f6
rodrigcasio <rod.casio@outlook.com>
2025-11-06 18:09:59 -0600

    Created example of using `try.. except` statement

--- courseraPractice/python101/exceptionHandlingEx.py
+++ courseraPractice/python101/exceptionHandlingEx.py
@@ -1,5 +1,16 @@
 # Try except:
 
+def save_divide(num, den):
+    try:
+        return num // den
+    except ZeroDivisionError:
+        print('Error: cannot divide by zero')
+        return none
+
+# test
+
+numerator = int(input('Enter the numerator value: -'))
+denominator = int(input('Enter the denominator value: -'))
 
 
-
+print(save_divide(numerator, denominator))
