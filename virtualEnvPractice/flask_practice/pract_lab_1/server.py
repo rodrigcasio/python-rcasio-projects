2d02146a0939e93782a9198b0f2f0e4429ee2c92
rodrigcasio <rod.casio@outlook.com>
2025-12-02 15:18:01 -0600

    `feat:` Add description for how to run the server correclty

--- virtualEnvPractice/flask_practice/pract_lab_1/server.py
+++ virtualEnvPractice/flask_practice/pract_lab_1/server.py
@@ -17,3 +17,10 @@ def index_1():
 @app.route('/hijson2')
 def index_2():
     return jsonify( message="Hello i was a string, but jsonify() helped me become JSON string")
+
+"""
+export FLASK_APP=server.py
+flask --app server --debug run (automatic resetting when changes made)
+curl -X GET -i -w '\n' localhost:5000
+
+Can be used, instead of adding within the string the "\n" in first response
