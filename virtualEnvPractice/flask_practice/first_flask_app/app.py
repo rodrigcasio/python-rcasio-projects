b193fd75da27b2b2bbe8f8e81f434a9b81ab2772
rodrigcasio <rod.casio@outlook.com>
2025-12-02 14:37:46 -0600

    `feat:` Add better instructions for `app.py` on how to run server

--- virtualEnvPractice/flask_practice/first_flask_app/app.py
+++ virtualEnvPractice/flask_practice/first_flask_app/app.py
@@ -26,4 +26,10 @@ flask run
 Running flask with automatic reset of server after changes made
 flask --app app.py --debug run
 (kinda like deamon module in node.js)
+
+can use:
+curl -X GET -i localhost:5000
+to obtain within the terminal, the response from the server
+
+-i : tells curl to include the HTTP response headers in the output
 """
