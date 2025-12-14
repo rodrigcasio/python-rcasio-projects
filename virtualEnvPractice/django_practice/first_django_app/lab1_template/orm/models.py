e20f4902e8060fbeb8aec4a4cf1d26a70b48ae55
rodrigcasio <rod.casio@outlook.com>
2025-12-13 19:23:20 -0600

    `feat:` Add django model named `User`

--- virtualEnvPractice/django_practice/first_django_app/lab1_template/orm/models.py
+++ virtualEnvPractice/django_practice/first_django_app/lab1_template/orm/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+# This is the first model ORM helps in creating this into a Database Table named User (Model)
 
-
-# Define your first model from here:
+class User(models.Model):
+    first_name = models.CharField(null = False, max_length = 30, default = 'john')
+    last_name = models.CharField(null = False, max_length = 30, default = 'doe')
+    dob = models.CharField(null = True)
