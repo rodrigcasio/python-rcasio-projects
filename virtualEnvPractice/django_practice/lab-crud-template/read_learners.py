00526c03d88e83737023a92a5149a3485baa06d2
rodrigcasio <rod.casio@outlook.com>
2025-12-16 02:13:50 -0600

    `feat:` Add query objects for Learner model with filters

--- virtualEnvPractice/django_practice/lab-crud-template/read_learners.py
+++ virtualEnvPractice/django_practice/lab-crud-template/read_learners.py
@@ -11,4 +11,16 @@ application = get_wsgi_application()
 from crud.models import *
 from datetime import date
 
+# find students with last name "Smith"
+smith_learner = Learner.objects.get(last_name='Smith')
+print(f"1. Find learner with last name 'Smith': {smith_learner}")
+print('\n')
+
+# Order by bod descending, and select the first dob two objects
+learners = Learner.objects.order_by('-dob')[0:2]
+print(f"2. Find top two yongest learners: {learners}")
+
+"""
+OUTPUT:
+
 
