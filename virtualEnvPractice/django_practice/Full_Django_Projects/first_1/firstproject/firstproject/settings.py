114b9cc0b689fa3984755711e77d26729d62694e
rodrigcasio <rod.casio@outlook.com>
2025-12-17 17:34:40 -0600

    `feat:` Include `firstapp/` into `firstproject/` within `settings.py`. Include import `include` and new path entry within `firstproject/urls.py`

--- virtualEnvPractice/django_practice/Full_Django_Projects/first_1/firstproject/firstproject/settings.py
+++ virtualEnvPractice/django_practice/Full_Django_Projects/first_1/firstproject/firstproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'firstapp.apps.FirstappConfig',  # included firstapp/ into firstproject
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
