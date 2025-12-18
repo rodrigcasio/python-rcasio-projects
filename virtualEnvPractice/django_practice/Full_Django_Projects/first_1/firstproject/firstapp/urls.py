20f8d28e8000f6d88db36e9093d7738559aa40e3
rodrigcasio <rod.casio@outlook.com>
2025-12-17 19:11:10 -0600

    `feat:` Add `View` to return current date
    Add view function `get_date()` in `firstapp/views.py`. Add URL path `/date` to `firstapp/urls.py`

--- virtualEnvPractice/django_practice/Full_Django_Projects/first_1/firstproject/firstapp/urls.py
+++ virtualEnvPractice/django_practice/Full_Django_Projects/first_1/firstproject/firstapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     # Create a path object defining URL patter to the index view.
     path(route='', view=views.index, name='index'),
+    path(route='date', view=views.get_date, name='date'),
 ]
--- virtualEnvPractice/django_practice/Full_Django_Projects/first_1/firstproject/firstapp/views.py
+++ virtualEnvPractice/django_practice/Full_Django_Projects/first_1/firstproject/firstapp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import date
+
+# views:
+# 1. index
+# 2. get_date
 
 def index(request):
     # create simple html page as a string
@@ -8,5 +13,14 @@ def index(request):
                 "</html>"
 
     return HttpResponse(content=template)
+
+def get_date(request):
+    today = date.today()
+    template = "<html>" \
+                f"Today's date is {today}" \
+                "</html>"
+
+    return HttpResponse(content=template)
+
 
 
