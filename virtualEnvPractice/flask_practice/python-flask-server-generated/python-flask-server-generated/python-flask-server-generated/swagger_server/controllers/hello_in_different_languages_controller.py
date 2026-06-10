e9cc2ec3c63f37915d5064a820c80ae2e22afc60
rodrigcasio <rod.casio@outlook.com>
2026-06-10 17:28:34 -0600

    `feat:` Add dictionary with multiple `hello`s in differnet languages

--- virtualEnvPractice/flask_practice/python-flask-server-generated/python-flask-server-generated/python-flask-server-generated/swagger_server/controllers/hello_in_different_languages_controller.py
+++ virtualEnvPractice/flask_practice/python-flask-server-generated/python-flask-server-generated/python-flask-server-generated/swagger_server/controllers/hello_in_different_languages_controller.py
@@ -6,10 +6,21 @@ from swagger_server import util
 
 def greetings_get():  # noqa: E501
     """Returns a list of Greetings
-
     Returns greetings in different languages # noqa: E501
-
-
-    :rtype: None
     """
-    return 'do some magic!'
+    hellos = {
+        "English": "hello",
+        "Hindi": "namastey",
+        "Spanish": "hola",
+        "French": "bonjour",
+        "German": "guten tag",
+        "Italian": "salve",
+        "Chinese": "nǐn hǎo",
+        "Portuguese": "olá",
+        "Arabic": "asalaam alaikum",
+        "Japanese": "konnichiwa",
+        "Korean": "anyoung haseyo",
+        "Russian": "Zdravstvuyte",
+    }
+
+    return hellos
